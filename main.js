ca031eaad293430ce034eafa6d9e3c8267f97ead
Jorge Luiz <jorgeluiz1163@gmail.com>
2022-05-08 17:46:13 -0300

    update

--- main.js
+++ main.js
@@ -1,3 +1,18 @@
-function onScroll(){
-    
+function onScroll() {
+console.log(scrollY > 0)
+    if (scrollY > 0) {
+        navigation.classList.add('scroll')
+   } else{
+       navigation.classList.remove('scroll')
+   }
+
 }
+
+function openMenu(){
+    document.body.classList.add('menu-expanded')
+}
+
+function closeMenu(){
+    document.body.classList.remove('menu-expanded')
+}
+
