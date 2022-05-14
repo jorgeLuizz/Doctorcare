60ec7e6d2aa3b41ce6bbe90491b915de75356c9c
Jorge Luiz <jorgeluiz1163@gmail.com>
2022-05-13 21:02:30 -0300

    update

--- main.js
+++ main.js
@@ -1,18 +1,24 @@
 function onScroll() {
-console.log(scrollY > 0)
+    console.log(scrollY > 0)
     if (scrollY > 0) {
         navigation.classList.add('scroll')
-   } else{
-       navigation.classList.remove('scroll')
-   }
+    } else {
+        navigation.classList.remove('scroll')
+    }
 
 }
 
-function openMenu(){
+function openMenu() {
     document.body.classList.add('menu-expanded')
 }
 
-function closeMenu(){
+function closeMenu() {
     document.body.classList.remove('menu-expanded')
 }
 
+
+ScrollReveal({
+    origin:'top',
+    distance:'30px',
+    duration: 700,
+}).reveal('#home');
